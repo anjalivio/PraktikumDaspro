836fea2be679c4b593b56cc232c6d76782d60206
anjalivio <anjaliviolita39@gmail.com>
2024-09-06 09:35:08 +0700

    praktikum minggu pertama

---
+++ Praktek/Latihan04.java
@@ -0,0 +1,14 @@
+/**
+ * Latihan04
+ */
+public class Latihan04 {
+
+    public static void main(String[] args){
+         System.out.println("***** IDENTITAS DIRI *****");
+         System.out.println("Nama     : Anjali Violita Pramestri");
+         System.out.println("Absen    : 04");
+         System.out.println("Kelas    : 1D");
+         System.out.println("Prodi    : Sistem Informasi Bisnis");
+         System.out.println("Jurusan  : Teknologi Informasi");
+    }
+}
